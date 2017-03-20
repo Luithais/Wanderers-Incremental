aa2231875860c9ddd65325a4584c5f3e30ccd6f8
Luthais Eventide <Luthais Eventide>
2017-03-20 07:16:26 +0000

    added .gitignore, setup VS Code Chrome Debugger

--- main.js
+++ main.js
@@ -1,47 +1,46 @@
-var playerGold = 0;
-var playerEquipment = ["head", "shoulders", "chest", "legs", "feet"];
-var clickGoldAmount = 1;
+(function(){ "use strict";
 
-var goblins = 0;
-var goblinCost = Math.floor(10 * Math.pow(1.1,goblins));     //works out the cost of this goblin
+    var playerGold = 0;
+    var playerEquipment = ["head", "shoulders", "chest", "legs", "feet"];
+    var clickGoldAmount = 1;
 
-var gps = function (gold) {
-    
-}
+    var goblins = 0;
+    var goblinCost = Math.floor(10 * Math.pow(1.1,goblins));     //works out the cost of this goblin
 
-var clickGold = function(clickGoldAmount)
-{
-   playerGold = playerGold + clickGoldAmount;
-};
-    
-$("#goldBtn").click(function(){
-        clickGold(clickGoldAmount);
-        $("#goldCount span").text(playerGold);
-    }
-);
-
-$("#goblinHireBtn").click(function () { 
-
-    if(playerGold >= goblinCost){                 //checks that the player can afford the goblin
-        if (goblins === 9) {
-        alert("Migs the Mighty joins your cause!");
-        };
-    
-        goblins++;                               //increases number of goblins
-    	playerGold = playerGold - goblinCost;    //removes the gold spent
-        $("#goblins span").text(goblins);  //updates the number of goblins for the user
-        $("#goldCount span").text(playerGold);  //updates the number of gold for the user
-
+    var clickGold = function(clickGoldAmount)
+    {
+    playerGold = playerGold + clickGoldAmount;
     };
+        
+    $("#goldBtn").click(function(){
+            clickGold(clickGoldAmount);
+            $("#goldCount span").text(playerGold);
+        }
+    );
 
-    goblinCost = Math.floor(10 * Math.pow(1.1,goblins));       //works out the cost of the next goblin
-    $("#goblinCost span").text(goblinCost); //updates the goblin cost for the user
-});
+    $("#goblinHireBtn").click(function () { 
+
+        if(playerGold >= goblinCost){                 //checks that the player can afford the goblin
+            if (goblins === 9) {
+            alert("Migs the Mighty joins your cause!");
+            }
+        
+            goblins++;                               //increases number of goblins
+            playerGold = playerGold - goblinCost;    //removes the gold spent
+            $("#goblins span").text(goblins);  //updates the number of goblins for the user
+            $("#goldCount span").text(playerGold);  //updates the number of gold for the user
+
+        }
+
+        goblinCost = Math.floor(10 * Math.pow(1.1,goblins));       //works out the cost of the next goblin
+        $("#goblinCost span").text(goblinCost); //updates the goblin cost for the user
+    });
 
 
 
 
-window.setInterval(function () {
-    clickGold(goblins);
-    $("#goldCount span").text(playerGold);
-}, 1000);
+    window.setInterval(function () {
+        clickGold(goblins);
+        $("#goldCount span").text(playerGold);
+    }, 1000);
+ }());
