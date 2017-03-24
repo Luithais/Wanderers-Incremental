2834856eed412db883810c9abbffebc406cc5b9e
Luthais Eventide <Luthais Eventide>
2017-03-24 05:34:09 +0000

    Changed emerald to tree, tidied HTML, CSS and JS. Left in broken purchaseUnit function.

--- main.js
+++ main.js
@@ -1,11 +1,24 @@
 (function(){ "use strict";
-
     var playerGold = 0;
-    var playerEquipment = ["head", "shoulders", "chest", "legs", "feet"];
     var clickGoldAmount = 1;
 
     var goblins = 0;
-    var goblinCost = Math.floor(10 * Math.pow(1.1,goblins));     //works out the cost of this goblin
+    var goblinCost = 10;     
+   
+        /* if (unitType === 9) {                        //checks if player has bought 9 goblins...
+            alert("Migs the Mighty joins your cause!"); //...'adds' Migs the Mighty to adventurers
+        }*/
+        
+    var purchaseUnit = function (unitType, unitCost) {
+         if(playerGold >= unitCost){                                     //checks that the player can afford the goblin
+            unitType++;                                                  //increases number of goblins
+            playerGold = playerGold - unitCost;                         //removes the gold spent
+            $("#goblins span").text(unitType);                          //updates the number of goblins for the user
+            $("#goldCount span").text(playerGold);                       //updates the number of gold for the user
+            $("#goblinCost span").text(unitCost);                     //updates the goblin cost for the user
+            unitCost = Math.floor(10 * Math.pow(1.1,goblins));
+        }
+    };
 
     var clickGold = function(clickGoldAmount)
     {
@@ -19,28 +32,13 @@
     );
 
     $("#goblinHireBtn").click(function () { 
-
-        if(playerGold >= goblinCost){                 //checks that the player can afford the goblin
-            if (goblins === 9) {
-            alert("Migs the Mighty joins your cause!");
-            }
-        
-            goblins++;                               //increases number of goblins
-            playerGold = playerGold - goblinCost;    //removes the gold spent
-            $("#goblins span").text(goblins);  //updates the number of goblins for the user
-            $("#goldCount span").text(playerGold);  //updates the number of gold for the user
-
-        }
-
-        goblinCost = Math.floor(10 * Math.pow(1.1,goblins));       //works out the cost of the next goblin
-        $("#goblinCost span").text(goblinCost); //updates the goblin cost for the user
+        purchaseUnit(goblins, goblinCost);
     });
 
-
-
-
+    //Game loop
     window.setInterval(function () {
         clickGold(goblins);
         $("#goldCount span").text(playerGold);
     }, 1000);
+
  }());
