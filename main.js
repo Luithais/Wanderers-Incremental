9d35597986c7fecfc9c04dd859efe5dc17e697b0
Luithais <volcarrona@gmail.com>
2017-03-24 18:12:57 +0000

    Update main.js

--- main.js
+++ main.js
@@ -2,21 +2,21 @@
     var playerGold = 0;
     var clickGoldAmount = 1;
 
-    var goblins = 0;
-    var goblinCost = 10;     
+    var goblin = {count:0, cost:10};
+         
    
-        /* if (unitType === 9) {                        //checks if player has bought 9 goblins...
+        /* if (unitType === 9) {                        //checks if player has bought 9 ...
             alert("Migs the Mighty joins your cause!"); //...'adds' Migs the Mighty to adventurers
         }*/
         
-    var purchaseUnit = function (unitType, unitCost) {
-         if(playerGold >= unitCost){                                     //checks that the player can afford the goblin
-            unitType++;                                                  //increases number of goblins
-            playerGold = playerGold - unitCost;                         //removes the gold spent
-            $("#goblins span").text(unitType);                          //updates the number of goblins for the user
+    var purchaseUnit = function (unit) {
+         if(playerGold >= unit.cost){                                     //checks that the player can afford the goblin
+            unit.count++;                                                  //increases number of goblins
+            playerGold = playerGold - unit.cost;                         //removes the gold spent
+            $("#goblins span").text(unit.count);                          //updates the number of goblins for the user
             $("#goldCount span").text(playerGold);                       //updates the number of gold for the user
-            $("#goblinCost span").text(unitCost);                     //updates the goblin cost for the user
-            unitCost = Math.floor(10 * Math.pow(1.1,goblins));
+            $("#goblinCost span").text(unit.cost);                     //updates the goblin cost for the user
+            unitCost = Math.floor(10 * Math.pow(1.1,goblin.count));
         }
     };
 
@@ -32,12 +32,12 @@
     );
 
     $("#goblinHireBtn").click(function () { 
-        purchaseUnit(&goblins, &goblinCost);
+        purchaseUnit(goblin);
     });
 
     //Game loop
     window.setInterval(function () {
-        clickGold(goblins);
+        clickGold(goblin.count);
         $("#goldCount span").text(playerGold);
     }, 1000);
 
