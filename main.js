cb639b7be2d1ccf023fb7b35e5eacc14336ee28b
Luithais <volcarrona@gmail.com>
2017-03-29 20:19:13 +0100

    Added new units, changed some other shite

--- main.js
+++ main.js
@@ -2,12 +2,13 @@
 
     /////   INITIALISE OBJECTS  /////
         var goblin = {count:0, cost:10, production: 1};
+        var faerie = {count:0, cost:200, production: 5};
+        var elf = {count:0, cost:750, production: 20};      
     /*------------------------------------------*/
 
     /////   INITIALISE VARIABLES    /////
         var playerGold = 0;
         var clickGoldAmount = 1;
-        var goblinProductionTotal = goblin.count * goblin.production;
     /*------------------------------------------*/
          
     /////   FUNCTIONS   /////    
@@ -15,7 +16,7 @@
             if(playerGold >= unit.cost){                                     //checks that the player can afford the goblin
                 unit.count++;                                                  //increases number of goblins
                 playerGold = playerGold - unit.cost;                         //removes the gold spent
-                unit.cost = Math.floor(10 * Math.pow(1.1,unit.count));
+                unit.cost = Math.floor(unit.cost * Math.pow(1.1,unit.count));
             }
         };
 
@@ -28,8 +29,6 @@
     /////   HTML CLICK EVENTS   ////
         //Tree
         $("#goldBtn").click(function(){
-                console.log(playerGold);
-                console.log(goblinProductionTotal);
                 addGold(clickGoldAmount);
                 $("#goldCount span").text(playerGold);
             }
@@ -38,17 +37,36 @@
         //Hire 1 Goblin
         $("#goblinHireBtn").click(function () { 
             purchaseUnit(goblin);
-            $("#goblins span").text(goblin.count);                          //updates the number of goblins for the user
+            $("#goblinCount span").text(goblin.count);                          //updates the number of goblins for the user
             $("#goldCount span").text(playerGold);                       //updates the number of gold for the user
             $("#goblinCost span").text(goblin.cost);                     //updates the goblin cost for the user
         });
+
+        //Hire 1 Fairy
+        $("#faerieHireBtn").click(function () { 
+            purchaseUnit(faerie);
+            $("#faerieCount span").text(faerie.count);                        
+            $("#goldCount span").text(playerGold);                       
+            $("#faerieCost span").text(faerie.cost);
+        });
+
+         //Hire 1 Elf
+        $("#elfHireBtn").click(function () { 
+            purchaseUnit(elf);
+            $("#elfCount span").text(elf.count);
+            $("#goldCount span").text(playerGold);                       
+            $("#elfCost span").text(elf.cost);                     
+        });
+
+        
 
     /*------------------------------------------*/
 
     /////   GAME LOOP   /////
         window.setInterval(function () {
-            goblinProductionTotal = goblin.count * goblin.production;
-            addGold(goblinProductionTotal);
+            addGold((goblin.count * goblin.production));
+            addGold((faerie.count * faerie.production));
+            addGold((elf.count * elf.production));
             $("#goldCount span").text(playerGold);
         }, 1000);
 
