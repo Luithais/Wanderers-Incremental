834cc67df8865be182169bde8da8c793a0301414
Luithais <volcarrona@gmail.com>
2018-03-13 18:14:18 +0000

    Workshop drag and drop system implemented, progression with layout

--- main.js
+++ main.js
@@ -12,18 +12,89 @@
     /*------------------------------------------*/
          
     /////   FUNCTIONS   /////    
-        var purchaseUnit = function (unit){                                    //Initialises purchaseUnit function, passes unit param
-            if(playerGold >= unit.cost){                                        //checks that the player can afford to purchase the unit
-                unit.count++;                                                   //adds 1 to unit count
-                playerGold = playerGold - unit.cost;                            //removes the gold spent
-                unit.cost = Math.floor(unit.cost * Math.pow(1.1,unit.count));   //calculates new unit cost
+        // Purchase Unit
+            var purchaseUnit = function (unit){                                    //Initialises purchaseUnit function, passes unit param
+                if(playerGold >= unit.cost){                                        //checks that the player can afford to purchase the unit
+                    unit.count++;                                                   //adds 1 to unit count
+                    playerGold = playerGold - unit.cost;                            //removes the gold spent
+                    unit.cost = Math.floor(unit.cost * Math.pow(1.1,unit.count));   //calculates new unit cost
+                }
+            };
+
+        // Add Gold
+            var addGold = function(goldIncrease){                                  //Initialises addGold function, passes goldIncrease
+                playerGold = playerGold + goldIncrease;                             //adds goldIncrease to player's current gold
+            };
+
+        // Tabs
+            $("#workshopBtn").click({tabName:"workshop"}, function(evt, tabName) {
+
+                    // Declare all variables
+                    var i, tabcontent, tablinks;
+                
+                    // Get all elements with class="tabcontent" and hide them
+                    tabcontent = document.getElementsByClassName("tabcontent");
+                    for (i = 0; i < tabcontent.length; i++) {
+                        tabcontent[i].style.display = "none";
+                    }
+                
+                    // Get all elements with class="tablinks" and remove the class "active"
+                    tablinks = document.getElementsByClassName("tablinks");
+                    for (i = 0; i < tablinks.length; i++) {
+                        tablinks[i].className = tablinks[i].className.replace(" active", "");
+                    }
+                
+                    // Show the current tab, and add an "active" class to the button that opened the tab
+                    document.getElementById("workshop").style.display = "block";
+                    evt.currentTarget.className += " active";
+                }
+            )
+
+            $("#expBtn").click({tabName:"exploration"}, function(evt, tabName) {
+
+                // Declare all variables
+                var i, tabcontent, tablinks;
+            
+                // Get all elements with class="tabcontent" and hide them
+                tabcontent = document.getElementsByClassName("tabcontent");
+                for (i = 0; i < tabcontent.length; i++) {
+                    tabcontent[i].style.display = "none";
+                }
+            
+                // Get all elements with class="tablinks" and remove the class "active"
+                tablinks = document.getElementsByClassName("tablinks");
+                for (i = 0; i < tablinks.length; i++) {
+                    tablinks[i].className = tablinks[i].className.replace(" active", "");
+                }
+            
+                // Show the current tab, and add an "active" class to the button that opened the tab
+                document.getElementById("exploration").style.display = "block";
+                evt.currentTarget.className += " active";
             }
-        };
+        )
 
-        var addGold = function(goldIncrease){                                  //Initialises addGold function, passes goldIncrease
-            playerGold = playerGold + goldIncrease;                             //adds goldIncrease to player's current gold
-        };
+            $("#workersBtn").click({tabName:"workers"}, function(evt, tabName) {
 
+                // Declare all variables
+                var i, tabcontent, tablinks;
+            
+                // Get all elements with class="tabcontent" and hide them
+                tabcontent = document.getElementsByClassName("tabcontent");
+                for (i = 0; i < tabcontent.length; i++) {
+                    tabcontent[i].style.display = "none";
+                }
+            
+                // Get all elements with class="tablinks" and remove the class "active"
+                tablinks = document.getElementsByClassName("tablinks");
+                for (i = 0; i < tablinks.length; i++) {
+                    tablinks[i].className = tablinks[i].className.replace(" active", "");
+                }
+            
+                // Show the current tab, and add an "active" class to the button that opened the tab
+                document.getElementById("workers").style.display = "block";
+                evt.currentTarget.className += " active";
+            }
+        )
     /*------------------------------------------*/
 
     /////   HTML CLICK EVENTS   ////
