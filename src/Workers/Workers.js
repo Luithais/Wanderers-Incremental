1ced939d5d559f7a79520712573bb4c798450450
Luithais <volcarrona@gmail.com>
2018-03-16 22:19:06 +0000

    Solved previous issue, purchase button successfully increases count

--- src/Workers/Workers.js
+++ src/Workers/Workers.js
@@ -2,46 +2,63 @@ import React, { Component } from 'react';
 import Resources from '../Resources/Resources';
 import WorkersBackend from './WorkersBackend';
 
+
+
 class Workers extends Component{
     
     constructor(props) {
         super(props);
-        this.hireBtnClickHandler = this.hireBtnClickHandler.bind(this);
+        this.state={
+            workers:[
+                {name:"Goblins",count:0,cost:20},
+                {name:"Faerie",count:0,cost:200},
+                {name:"Elf",count:0,cost:1500},
+            ]
+        };
     };
     
    
 
-    state={
-        workers:[
-            {name:"Goblins",count:0,cost:20},
-            {name:"Faerie",count:0,cost:200},
-            {name:"Elf",count:0,cost:1500},
-        ]
-    };
+
+
+    //1. Get array
+    //2. Set index to required value
+    //3. Push full array to state
 
     hireBtnClickHandler = (id) => {
 
-        this.setState.workers[id] = this.state.workers[id].count + 1;
-        //newCost=0
+        var array=[...this.state.workers];
+        array[id].count = array[id].count + 1;
 
+        console.log(array[id]);
         this.setState(
             {
-              workers: [
-                  {name:"Goblins",count:this.state.workers[id].count,cost:20},
-                  {name:"Faerie",count:this.state.workers[id].count,cost:200},
-                  {name:"Elf",count:this.state.workers[id].count,cost:1500},
-              ]
+              workers: [array[0],array[1],array[2]]
             }
-          )
-      }
+        )
+    }
 
 
     render () {
         return(
             <div>
-                <WorkersBackend click={() => this.hireBtnClickHandler(0)} name={this.state.workers[0].name} cost={this.state.workers[0].cost} count={this.state.workers[0].count}/>
-                <WorkersBackend click={() => this.hireBtnClickHandler(1)} name={this.state.workers[1].name} cost={this.state.workers[1].cost} count={this.state.workers[1].count}/>
-                <WorkersBackend click={() => this.hireBtnClickHandler(2)} name={this.state.workers[2].name} cost={this.state.workers[2].cost} count={this.state.workers[2].count}/>
+                <WorkersBackend
+                    click={this.hireBtnClickHandler.bind(this,0)}
+                    name={this.state.workers[0].name}
+                    cost={this.state.workers[0].cost}
+                    count={this.state.workers[0].count}/>
+
+                <WorkersBackend
+                    click={this.hireBtnClickHandler.bind(this,1)}
+                    name={ this.state.workers[1].name}
+                    cost={ this.state.workers[1].cost}
+                    count={this.state.workers[1].count}/>
+
+                <WorkersBackend
+                    click={this.hireBtnClickHandler.bind(this,2)}
+                    name={this.state.workers[2].name}
+                    cost={this.state.workers[2].cost}
+                    count={ this.state.workers[2].count}/>
             </div>
         );
     };
